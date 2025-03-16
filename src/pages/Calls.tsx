680bac25b00d4d22d9fe1d547f8ef73979acbd06
j-tangpuz8 <jtangpuz@guardian.ph>
2025-03-16 22:55:17 +0800

    added layout to responder screen

--- src/pages/Calls.tsx
+++ src/pages/Calls.tsx
@@ -12,15 +12,14 @@ import {
   User,
 } from "@stream-io/video-react-sdk";
 // import CallContainer from "../components/CallContainer";
+import avatarImg from "../assets/images/avatar.jpg";
 import {useEffect, useState} from "react";
 import {useNavigate, useLocation} from "react-router-dom";
 import config from "../config";
 import "@stream-io/video-react-sdk/dist/css/styles.css";
-import {
-  MyFloatingLocalParticipant,
-  MyParticipantList,
-} from "../components/Participants";
-import {Button, Typography} from "@mui/material";
+import all from "../utils/Responders";
+import {Button, Paper, Typography} from "@mui/material";
+import WarningIcon from "@mui/icons-material/Warning";
 
 const userStr = localStorage.getItem("user");
 const userStr2 = userStr ? JSON.parse(userStr) : null;
@@ -84,17 +83,125 @@ export default function Calls() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-[#01041c]">
-      <StreamVideo client={client}>
-        <StreamCall call={call}>
-          <MyUILayout />
-        </StreamCall>
-      </StreamVideo>
+    <div className="flex h-screen bg-[#1B4965] p-5 sm:gap-10 md:gap-2">
+      <div className="w-[350px] bg-gray-300 rounded-lg">
+        <div className="flex items-center gap-4 p-5">
+          <div className="bg-green-200 p-2 rounded-full border-1">
+            <WarningIcon sx={{color: "maroon", fontSize: "3em"}} />
+          </div>
+          <div>
+            <h2 className="text-xl font-bold">ID: {call?.id}</h2>
+            <p className="text-green-700 font-bold text-lg">GENERAL CALL</p>
+            <p className="text-sm">A. S. Fortune St, Mandaue City</p>
+            <p className="text-sm">Coordinates: 10.343897, 123.932080</p>
+          </div>
+        </div>
+
+        <div className="my-1 bg-white p-5">
+          <div className="flex items-center gap-4">
+            <img
+              src={avatarImg}
+              alt="avatar"
+              className="w-20 h-20 rounded-full"
+            />
+            <div>
+              <h3 className="text-xl uppercase font-bold">{user?.name}</h3>
+              <p className="text-sm">1234567890</p>
+              <p className="text-sm">GuardianPH Opcen</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="p-5">
+          <label className="text-lg font-bold">Directory</label>
+          <input
+            type="text"
+            placeholder="Search"
+            className="w-full p-2 border rounded bg-white"
+          />
+        </div>
+
+        <div className="px-5 overflow-auto h-[460px]">
+          <div>
+            <label className="text-lg font-bold">Operation Centers</label>
+            {all.opCenters.map((opCenter, index) => (
+              <Paper
+                key={index}
+                elevation={2}
+                sx={{
+                  padding: "12px",
+                  cursor: "pointer",
+                  marginBottom: "1px",
+                  transition: "all 0.2s ease",
+                  backgroundColor: "white",
+                  "&:hover": {
+                    backgroundColor: "#f5f5f5",
+                    transform: "translateX(4px)",
+                  },
+                }}>
+                {opCenter}
+              </Paper>
+            ))}
+          </div>
+
+          <div className="mt-5">
+            <label className="text-lg font-bold">Police Stations</label>
+            {all.policeStations.map((stations, index) => (
+              <Paper
+                key={index}
+                elevation={2}
+                sx={{
+                  padding: "12px",
+                  cursor: "pointer",
+                  marginBottom: "1px",
+                  transition: "all 0.2s ease",
+                  backgroundColor: "white",
+                  "&:hover": {
+                    backgroundColor: "#f5f5f5",
+                    transform: "translateX(4px)",
+                  },
+                }}>
+                {stations}
+              </Paper>
+            ))}
+          </div>
+          <div className="mt-5">
+            <label className="text-lg font-bold">Hospitals</label>
+            {all.hospitals.map((hospital, index) => (
+              <Paper
+                key={index}
+                elevation={2}
+                sx={{
+                  padding: "12px",
+                  cursor: "pointer",
+                  marginBottom: "1px",
+                  transition: "all 0.2s ease",
+                  backgroundColor: "white",
+                  "&:hover": {
+                    backgroundColor: "#f5f5f5",
+                    transform: "translateX(4px)",
+                  },
+                }}>
+                {hospital}
+              </Paper>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      {/* video call*/}
+      <div className="flex-1">
+        <StreamVideo client={client}>
+          <StreamCall call={call}>
+            <VideoCall />
+          </StreamCall>
+        </StreamVideo>
+      </div>
     </div>
   );
 }
 
-export const MyUILayout = () => {
+export const VideoCall = () => {
   const call = useCall();
   const navigate = useNavigate();
 
@@ -144,18 +251,18 @@ export const MyUILayout = () => {
           Leave Call
         </Button>
         <Typography variant="h6" color="white">
-          There are {participantCount} participants in this call
+          Participants in this call: {participantCount}
         </Typography>
       </div>
-      <div className="flex flex-col items-center justify-center">
+      <div className="flex gap-5 items-center justify-center">
         <Button
           variant="contained"
-          color="primary"
-          onClick={() => handleRemoveParticipant()}>
-          Remove Other Participant
+          onClick={() => handleRemoveParticipant()}
+          sx={{backgroundColor: "white", color: "maroon"}}>
+          Remove Caller
         </Button>
-        <Button variant="contained" color="primary" sx={{marginTop: "10px"}}>
-          Let Other Participant Join
+        <Button variant="contained">
+          <span className="text-xl">+&nbsp;</span> Invite Participant
         </Button>
       </div>
     </StreamTheme>
